ab924404258b59d4140c13e7da1434d61fa93052
edito13 <editotchocosso13@gmail.com>
2025-11-11 23:00:02 +0100

    using a customExpire to generate token

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -7,8 +7,13 @@ dotenv.config();
 const secret = process.env.JWT_SECRET as string;
 const expiresIn = process.env.JWT_EXPIRES_IN as Jwt.SignOptions["expiresIn"];
 
-const generateToken = (data: UserData) => {
-  const token = Jwt.sign(data, secret, { expiresIn });
+const generateToken = (
+  data: UserData,
+  customExpire?: Jwt.SignOptions["expiresIn"]
+) => {
+  const token = Jwt.sign(data, secret, {
+    expiresIn: customExpire || expiresIn,
+  });
   return token;
 };
 
