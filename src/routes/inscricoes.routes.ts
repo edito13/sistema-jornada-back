98ac132d5f70cd8632094e59da8c02f192649497
edito13 <editotchocosso13@gmail.com>
2025-11-24 09:01:19 +0100

    create registrations endpoints

--- src/routes/inscricoes.routes.ts
+++ src/routes/inscricoes.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
 import {
-    inscrever_se,
-    cancelarInscricao,
-    listaInscricoes,
+  inscrever_se,
+  cancelarInscricao,
+  listaInscricoes,
 } from "../controllers/inscricoes.controller";
 
 const router = Router();
 
 router.post("/inscrever_se/:id", authMiddleware, inscrever_se);
-router.delete("/cancelarInscricao/:id", authMiddleware, cancelarInscricao);
 router.get("/listaInscricoes/:id", authMiddleware, listaInscricoes);
+router.delete("/cancelarInscricao/:id", authMiddleware, cancelarInscricao);
 
 export default router;
