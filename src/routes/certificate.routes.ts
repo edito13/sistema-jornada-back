1fc9bca3a1c70116be2b3e4a7354fb2a66de9256
edito13 <editotchocosso13@gmail.com>
2026-05-13 12:22:28 +0100

    create certificate route

--- src/routes/certificate.routes.ts
+++ src/routes/certificate.routes.ts
@@ -1,64 +1,16 @@
 import { Router } from "express";
-import PDFDocument from "pdfkit";
-import { PassThrough } from "stream";
-import { getCertificate } from "../controllers/certificate.controller";
+import {
+  getCertificate,
+  listCertificates,
+  listUserCertificates,
+} from "../controllers/certificate.controller";
+import authMiddleware from "../middlewares/auth.middleware";
+import adminMiddleware from "../middlewares/admin.middleware";
 
 const router = Router();
 
-router.get("/:id_inscricao", getCertificate);
-router.get("/usuario/:id_user", (req, res) => res.json([]));
-
-router.get("/", async (req, res) => {
-  const doc = new PDFDocument({
-    size: "A4",
-    layout: "landscape",
-    margin: 50,
-  });
-
-  // Criar stream para capturar os bytes do PDF
-  const stream = new PassThrough();
-  const chunks: Buffer[] = [];
-
-  doc.pipe(stream);
-  stream.on("data", (chunk) => chunks.push(chunk));
-
-  const endPromise = new Promise<Buffer>((resolve) => {
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-  });
-
-  // --- Conteúdo do certificado ---
-  doc.fontSize(30).fillColor("#333").text("Certificado de Participação", {
-    align: "center",
-  });
-
-  doc.moveDown(2);
-  doc
-    .fontSize(20)
-    .text("Certificamos que Edito Tchokoso", { align: "center" })
-    .moveDown(1)
-    .text('participou do evento "Node.js Express Workshop".', {
-      align: "center",
-    })
-    .moveDown(1)
-    .text(`Realizado em ${new Date().toLocaleDateString("pt-PT")}.`, {
-      align: "center",
-    });
-
-  doc.moveDown(4);
-  doc.text("_______________________", 150, 400);
-  doc.text("Organizador", 180, 420);
-
-  doc.end();
-
-  // Esperar o PDF ser gerado
-  const pdfBuffer = await endPromise;
-
-  res.setHeader("Content-Type", "application/pdf");
-  res.setHeader(
-    "Content-Disposition",
-    `attachment; filename=certificado_teste.pdf`,
-  );
-  res.send(pdfBuffer);
-});
+router.get("/", authMiddleware, adminMiddleware, listCertificates);
+router.get("/usuario/:id_user", authMiddleware, listUserCertificates);
+router.get("/:id_inscricao", authMiddleware, getCertificate);
 
 export default router;
